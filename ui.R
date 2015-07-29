bffcc9cb5dcba4156e9fe5c77b456b5ac5886796
Amanpreet Singh <mngujral@gmail.com>
2015-07-29 17:02:05 +0530

    ui.R
    ui.R 7/29 1700

---
+++ ui.R
@@ -0,0 +1,25 @@
+library(shiny)
+
+shinyUI(fluidPage(
+
+  headerPanel("Twitter Analysis"),
+  
+  sidebarPanel( textInput("term", "Enter a term", ""),
+                sliderInput("count", 
+                            label = "Number of tweets to load:",
+                            min = 0, max = 200, value = 0),
+                numericInput("obs", "Number of tweets to view:", 5),
+                selectInput("lang","Select the language",
+                             c("English"="en",
+                               "French"="fr",
+                               "Spanish"="es"), selected = "en"),
+#                 dateInput("date", "Since :", 
+#                           value = "2013-01-01"),
+                  submitButton(text="Run")
+  ),
+  
+  mainPanel(
+    h4("Last few Tweets"),
+    tableOutput("table"),
+    plotOutput("wordcl"))
+))
